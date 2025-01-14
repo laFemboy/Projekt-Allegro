2b848097735273791ec816cafa2b47025a68b353
ShHryhorii <163681108+ShHryhorii@users.noreply.github.com>
2025-01-14 21:06:06 +0100

    initial backend commit

---
+++ allegro_backend/src/main/java/com/example/allegro_backend/AllegroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.allegro_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllegroBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllegroBackendApplication.class, args);
+    }
+
+}
---
+++ allegro_backend/src/test/java/com/example/allegro_backend/AllegroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.allegro_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllegroBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
